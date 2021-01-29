f6025972ecf13368da13996213929bafbb501063
Zola Gonano <zolagonano@protonmail.com>
2021-01-29 10:39:14 -0700

    CHANGE: Format with cargo fmt

--- src/ciphers/rot.rs
+++ src/ciphers/rot.rs
@@ -1,25 +1,25 @@
-pub enum RotType{
+pub enum RotType {
     Rot5,
     Rot13,
     Rot18,
     Rot47,
 }
 
-pub struct Rot<'a>{
+pub struct Rot<'a> {
     message: &'a str,
     rot_type: RotType,
 }
 
-impl Rot<'_>{
-    pub fn new(message: &str, rot_type: RotType) -> Rot{
-        Rot{
+impl Rot<'_> {
+    pub fn new(message: &str, rot_type: RotType) -> Rot {
+        Rot {
             message: message,
             rot_type: rot_type,
         }
     }
 
-    pub fn encipher(&self) -> String{
-        match self.rot_type{
+    pub fn encipher(&self) -> String {
+        match self.rot_type {
             RotType::Rot5 => return Rot::rot5(self.message),
             RotType::Rot13 => return Rot::rot13(self.message),
             RotType::Rot18 => return Rot::rot13(&(Rot::rot5(self.message))),
@@ -27,43 +27,46 @@ impl Rot<'_>{
         }
     }
 
-    pub fn decipher(&self) -> String{
+    pub fn decipher(&self) -> String {
         self.encipher()
     }
 
-    fn rot5(message: &str) -> String{
+    fn rot5(message: &str) -> String {
         message
             .chars()
-            .map(|digit| match digit{
+            .map(|digit| match digit {
                 '0'..='4' => ((digit as u8) + 5) as char,
                 '5'..='9' => ((digit as u8) - 5) as char,
                 _ => digit,
-            }).collect()
+            })
+            .collect()
     }
 
-    fn rot13(message: &str) -> String{
+    fn rot13(message: &str) -> String {
         message
             .chars()
-            .map(|character| match character{
+            .map(|character| match character {
                 'A'..='M' | 'a'..='m' => ((character as u8) + 13) as char,
                 'N'..='Z' | 'n'..='z' => ((character as u8) - 13) as char,
                 _ => character,
-            }).collect()
+            })
+            .collect()
     }
 
-    fn rot47(message: &str) -> String{
+    fn rot47(message: &str) -> String {
         message
             .chars()
-            .map(|character| match character{
+            .map(|character| match character {
                 '!'..='O' => ((character as u8) + 47) as char,
                 'P'..='~' => ((character as u8) - 47) as char,
                 _ => character,
-            }).collect()
+            })
+            .collect()
     }
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::{Rot, RotType};
 
     #[test]
@@ -80,14 +83,12 @@ mod tests{
         assert_eq!(String::from("• Hello Friend 83110 :) •"), rot47.decipher());
     }
 
-
     #[test]
     fn rot18_encipher() {
         let rot18 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot18);
 
         assert_eq!(String::from("• Uryyb Sevraq 38665 :) •"), rot18.encipher());
     }
-
 
     #[test]
     fn rot18_decipher() {
@@ -96,14 +97,12 @@ mod tests{
         assert_eq!(String::from("• Hello Friend 83110 :) •"), rot18.decipher());
     }
 
-
     #[test]
     fn rot13_encipher() {
         let rot13 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot13);
 
         assert_eq!(String::from("• Uryyb Sevraq 83110 :) •"), rot13.encipher());
     }
-
 
     #[test]
     fn rot13_decipher() {
@@ -112,14 +111,12 @@ mod tests{
         assert_eq!(String::from("• Hello Friend 83110 :) •"), rot13.decipher());
     }
 
-
     #[test]
     fn rot5_encipher() {
         let rot5 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot5);
 
         assert_eq!(String::from("• Hello Friend 38665 :) •"), rot5.encipher());
     }
-
 
     #[test]
     fn rot5_decipher() {
