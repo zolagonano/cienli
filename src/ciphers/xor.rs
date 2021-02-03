148eeeb1e688ab86b1553d500fbf6bcb6572195e
Zola Gonano <zolagonano@protonmail.com>
2021-02-03 06:15:46 -0700

    ADD: Add Documention

--- src/ciphers/xor.rs
+++ src/ciphers/xor.rs
@@ -1,20 +1,48 @@
 use crate::common::key_gen;
 
+/// Xor Cipher
+///
+/// The struct is generated through the new() function.
+///
 pub struct Xor<'a> {
     key: &'a str,
 }
 
 impl Xor<'_> {
+    /// Initialize a xor cipher with the key.
+    ///
+    /// # Examples:
+    /// ```
+    /// use cienli::ciphers::xor::Xor;
+    /// let xor = Xor::new("VMMN8");
+    /// ```
     pub fn new(key: &str) -> Xor {
         Xor { key: key }
     }
 
+    /// Enciphers a message with the xor cipher.
+    ///
+    /// # Examples:
+    /// ```
+    /// use cienli::ciphers::xor::Xor;
+    /// let xor = Xor::new("VMMN8");
+    ///
+    /// assert_eq!("<=|zv", xor.encipher("jp14N"));
+    /// ```
     pub fn encipher(&self, message: &str) -> String {
         let key = key_gen(&self.key, message.len()).unwrap();
 
         Xor::xor_engine(message, &key)
     }
 
+    /// Deciphers a cipher with the xor cipher.
+    ///
+    /// # Examples:
+    /// ```
+    /// use cienli::ciphers::xor::Xor;
+    /// let xor = Xor::new("VMMN8");
+    /// assert_eq!("jp14N", xor.decipher("<=|zv"));
+    /// ```
     pub fn decipher(&self, cipher: &str) -> String {
         let key = key_gen(&self.key, cipher.len()).unwrap();
 
@@ -43,13 +71,13 @@ mod tests {
     fn encipher() {
         let xor = Xor::new("VMMN8");
 
-        assert_eq!(String::from("<=|zv"), xor.encipher("jp14N"));
+        assert_eq!("<=|zv", xor.encipher("jp14N"));
     }
 
     #[test]
     fn decipher() {
-        let xor = Xor::new("jp14N");
+        let xor = Xor::new("VMMN8");
 
-        assert_eq!(String::from("VMMN8"), xor.decipher("<=|zv"));
+        assert_eq!("jp14N", xor.decipher("<=|zv"));
     }
 }
