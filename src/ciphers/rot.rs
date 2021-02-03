592380cdf9f4b680fd7859a03b2d41c309e72a38
Zola Gonano <zolagonano@protonmail.com>
2021-02-03 06:15:35 -0700

    ADD: Add Documention

--- src/ciphers/rot.rs
+++ src/ciphers/rot.rs
@@ -5,12 +5,24 @@ pub enum RotType {
     Rot47,
 }
 
+/// Rot Cipher
+///
+/// The struct is generated through the new() function.
+///
 pub struct Rot<'a> {
     message: &'a str,
     rot_type: RotType,
 }
 
 impl Rot<'_> {
+    /// Initialize a rot cipher with a message and rot type.
+    ///
+    /// # Examples:
+    /// - Initialization with Rot13 type.:
+    /// ```
+    /// use cienli::ciphers::rot::{Rot, RotType};
+    /// let rot = Rot::new("• Hello Friend 83110 :) •", RotType::Rot13);
+    /// ```
     pub fn new(message: &str, rot_type: RotType) -> Rot {
         Rot {
             message: message,
@@ -18,6 +30,25 @@ impl Rot<'_> {
         }
     }
 
+    /// Enciphers a message with the rot cipher.
+    ///
+    /// # Examples:
+    ///
+    /// - Encipher with Rot47:
+    /// ```
+    /// use cienli::ciphers::rot::{Rot, RotType};
+    /// let rot47 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot47);
+    ///
+    /// assert_eq!("• w6==@ uC:6?5 gb``_ iX •", rot47.encipher());
+    /// ```
+    ///
+    /// - Encipher with Rot13:
+    /// ```
+    /// use cienli::ciphers::rot::{Rot, RotType};
+    /// let rot13 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot13);
+    ///
+    /// assert_eq!("• Uryyb Sevraq 83110 :) •", rot13.encipher());
+    /// ```
     pub fn encipher(&self) -> String {
         match self.rot_type {
             RotType::Rot5 => return Rot::rot5(self.message),
@@ -27,6 +58,25 @@ impl Rot<'_> {
         }
     }
 
+    /// Deciphers a cipher with the rot cipher.
+    ///
+    /// # Examples:
+    ///
+    /// - Decipher with Rot47:
+    /// ```
+    /// use cienli::ciphers::rot::{Rot, RotType};
+    /// let rot47 = Rot::new("• w6==@ uC:6?5 gb``_ iX •", RotType::Rot47);
+    ///
+    /// assert_eq!("• Hello Friend 83110 :) •", rot47.decipher());
+    /// ```
+    ///
+    /// - Decipher with Rot13:
+    /// ```
+    /// use cienli::ciphers::rot::{Rot, RotType};
+    /// let rot13 = Rot::new("• Uryyb Sevraq 83110 :) •", RotType::Rot13);
+    ///
+    /// assert_eq!("• Hello Friend 83110 :) •", rot13.decipher());
+    /// ```
     pub fn decipher(&self) -> String {
         self.encipher()
     }
@@ -73,55 +123,55 @@ mod tests {
     fn rot47_encipher() {
         let rot47 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot47);
 
-        assert_eq!(String::from("• w6==@ uC:6?5 gb``_ iX •"), rot47.encipher());
+        assert_eq!("• w6==@ uC:6?5 gb``_ iX •", rot47.encipher());
     }
 
     #[test]
     fn rot47_decipher() {
         let rot47 = Rot::new("• w6==@ uC:6?5 gb``_ iX •", RotType::Rot47);
 
-        assert_eq!(String::from("• Hello Friend 83110 :) •"), rot47.decipher());
+        assert_eq!("• Hello Friend 83110 :) •", rot47.decipher());
     }
 
     #[test]
     fn rot18_encipher() {
         let rot18 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot18);
 
-        assert_eq!(String::from("• Uryyb Sevraq 38665 :) •"), rot18.encipher());
+        assert_eq!("• Uryyb Sevraq 38665 :) •", rot18.encipher());
     }
 
     #[test]
     fn rot18_decipher() {
         let rot18 = Rot::new("• Uryyb Sevraq 38665 :) •", RotType::Rot18);
 
-        assert_eq!(String::from("• Hello Friend 83110 :) •"), rot18.decipher());
+        assert_eq!("• Hello Friend 83110 :) •", rot18.decipher());
     }
 
     #[test]
     fn rot13_encipher() {
         let rot13 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot13);
 
-        assert_eq!(String::from("• Uryyb Sevraq 83110 :) •"), rot13.encipher());
+        assert_eq!("• Uryyb Sevraq 83110 :) •", rot13.encipher());
     }
 
     #[test]
     fn rot13_decipher() {
         let rot13 = Rot::new("• Uryyb Sevraq 83110 :) •", RotType::Rot13);
 
-        assert_eq!(String::from("• Hello Friend 83110 :) •"), rot13.decipher());
+        assert_eq!("• Hello Friend 83110 :) •", rot13.decipher());
     }
 
     #[test]
     fn rot5_encipher() {
         let rot5 = Rot::new("• Hello Friend 83110 :) •", RotType::Rot5);
 
-        assert_eq!(String::from("• Hello Friend 38665 :) •"), rot5.encipher());
+        assert_eq!("• Hello Friend 38665 :) •", rot5.encipher());
     }
 
     #[test]
     fn rot5_decipher() {
         let rot5 = Rot::new("• Hello Friend 38665 :) •", RotType::Rot5);
 
-        assert_eq!(String::from("• Hello Friend 83110 :) •"), rot5.decipher());
+        assert_eq!("• Hello Friend 83110 :) •", rot5.decipher());
     }
 }
