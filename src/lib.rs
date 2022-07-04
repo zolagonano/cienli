2560023a3c03260f424ab6696525ff3a48e55e9b
Zola Gonano <zolagonano@protonmail.com>
2022-07-04 12:09:33 +0000

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
+//! An Implementation of historical ciphers.
+//!
+//! **Note:** If you only need to use one or a couple of ciphers, you can disable default features and specify those features you need.
+//! For example, if you only need to use caesar cipher in your crate, you can have this in your Cargo.toml:
+//!
+//! ```toml
+//! [dependencies]
+//! cienli = { version = "0.3.1", default-features = false, features = ["caesar"]}
+//! ```
+//!
 pub mod ciphers;
 pub mod common;
